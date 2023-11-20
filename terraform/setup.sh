a68affe32c9eef0126a76b625125a32cc2ea3235
kyuye <ivyheart@gmail.com>
2023-11-20 14:54:12 +0900

    deploy tested

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -7,4 +7,4 @@ sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 sudo git clone -b deploy-test https://github.com/leechanwoo/technopark-git-practice.git
-sudo docker compose -f $(pwd)/technopark-git-practice/docker-compose-deploy.yaml up -d
+sudo docker compose -f $(pwd)/technopark-git-practice/docker-compose-deploy.yml up -d
