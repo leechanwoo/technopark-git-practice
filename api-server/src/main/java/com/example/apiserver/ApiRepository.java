0274966bf3da2c71f5d0f675b2b1c690a9a633b4
kyuye <ivyheart@gmail.com>
2023-11-20 17:11:03 +0900

    grpc client implemented

--- api-server/src/main/java/com/example/apiserver/ApiRepository.java
+++ api-server/src/main/java/com/example/apiserver/ApiRepository.java
@@ -4,13 +4,21 @@ package com.example.apiserver;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Repository;
 import jakarta.annotation.PostConstruct;
 
+
 @Repository
 public class ApiRepository { 
     public ApiRepository() { }
+
+    public String stringify(List<java.lang.Float> numbers) {
+        return numbers.stream()
+                      .map(n -> String.valueOf(n))
+                      .collect(Collectors.joining(", ", "[", "]"));
+    }
     
 }
